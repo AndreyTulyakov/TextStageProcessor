80dfa180099b1af757433f20ec595e8fe94dfac8
DESKTOP-8USB258\Dmitry <megalord2@mail.ru>
2017-02-03 02:28:41 +0300

    добавлен второй метод

--- sources/TextClasterization.py
+++ sources/TextClasterization.py
@@ -5,6 +5,7 @@ import pymorphy2
 import math
 from pymorphy2 import tokenizers
 import os
+import random
 
 from PyQt5.QtWidgets import QDialog, QMessageBox, QTextEdit
 from PyQt5 import QtCore, QtGui, uic
@@ -87,7 +88,7 @@ def dist(w1, w2):
     return dist
 
 
-def FindUnion(Dist, F):
+def FindUnionDist(Dist, F):
     min_i = 0
     min_j = 0
     min = 1000
@@ -102,6 +103,20 @@ def FindUnion(Dist, F):
 
     return [min_i, min_j], Dist[min_i][min_j]
 
+def FindUnionSim(Sim, F):
+    max_i = 0
+    max_j = 0
+    max = 0
+    n = len(Sim)
+    m = len(Sim[0])
+    for i in range(n):
+        for j in range(i):
+            if ((Sim[i][j] > max) & (F[i] == 1) & (j < i)):
+                max = Sim[i][j]
+                max_i = i
+                max_j = j
+
+    return [max_i, max_j], Sim[max_i][max_j]
 
 class Cluster:
     ap = 1 / 2
@@ -176,6 +191,7 @@ class DialogConfigClasterization(QDialog):
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
 
         self.buttonClasterization.clicked.connect(self.makeClasterization)
+        self.pushButtonKMiddle.clicked.connect(self.makeClasterizationKMiddle)
         self.textEdit.setText("")
 
 
@@ -183,9 +199,9 @@ class DialogConfigClasterization(QDialog):
 
         self.textEdit.setText("")
         self.configurations["minimal_word_size"] = self.spinBoxMinimalWordsLen.value()
-
+        self.textEdit.append('Иерархическая кластеризация' + '\n')
         texts = makePreprocessing(self.filenames, self.morph, self.configurations, self.textEdit)
-        output_dir = self.configurations.get("output_files_directory", "output_files") + "/clasterization/" 
+        output_dir = self.configurations.get("output_files_directory", "output_files") + "/clasterization/Hierarchical/"
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         # Нахождение матрицы весов
@@ -259,18 +275,15 @@ class DialogConfigClasterization(QDialog):
 
         n = len(texts)
         m = len(texts)
-        dist = [[0 for x in range(n)] for y in range(m)]
+        S = [[0 for x in range(n)] for y in range(m)]
 
+        #Находим таблицу Dist
         for i in range(n):
             for j in range(m):
                 summ = 0
                 for k in range(len(t_all)):
                     summ += math.pow(W[i][k] - W[j][k], 2)
-                dist[i][j] = math.sqrt(summ)
-
-
-
-
+                S[i][j] = math.sqrt(summ)
 
         dist_string = ''
 
@@ -281,7 +294,7 @@ class DialogConfigClasterization(QDialog):
             dist_string += 'doc' + str(self.filenames[i])[
                                    str(self.filenames[i]).find('/') + 1:str(self.filenames[i]).find('.')]
             for j in range(len(texts)):
-                dist_string = dist_string + ';' + str(round(dist[i][j], 2))
+                dist_string = dist_string + ';' + str(round(S[i][j], 2))
             dist_string += '\n'
         writeStringToFile(dist_string.replace('\n ', '\n').replace('.', ','), output_dir + 'dist.csv')
 
@@ -293,16 +306,13 @@ class DialogConfigClasterization(QDialog):
         for i in range(len(texts)):
             clusters[i] = [i]
 
-
-
-
         F = [1 for x in range(len(texts))]
 
         result = ''
-
+        #Find unions with dist
         for k in range(len(texts) - 1):
-            printDist(dist, texts, self.filenames)
-            union, currDist = FindUnion(dist, F)
+            printDist(S, texts, self.filenames)
+            union, currDist = FindUnionDist(S, F)
             firstCluster = ClusterByDoc(union[0], clusters)
             secondCluster = ClusterByDoc(union[1], clusters)
             print('found clusters = ' + str(firstCluster) + ' and ' + str(secondCluster))
@@ -319,19 +329,249 @@ class DialogConfigClasterization(QDialog):
 
             F[union[0]] = 0
             # F[union[1]] = 0
-            new_dist = [[0 for x in range(len(texts))] for y in range(len(texts))]
+            new_sim = [[0 for x in range(len(texts))] for y in range(len(texts))]
             for i in range(len(texts)):
                 for j in range(len(texts)):
-                    new_dist[i][j] = dist[i][j]
+                    new_sim[i][j] = S[i][j]
 
             for j in range(len(texts)):
                 for i in range(2):
-                    new_dist[j][union[i]] = 0.5 * (dist[j][union[0]]) + 0.5 * (dist[j][union[1]])
+                    new_sim[j][union[i]] = 0.5 * (S[j][union[0]]) + 0.5 * (S[j][union[1]])
 
-            dist = new_dist
+            S = new_sim
 
-        writeStringToFile(result.replace('\n ', '\n'), output_dir + 'steps.csv')
+        writeStringToFile(result.replace('\n ', '\n'), output_dir + 'stepsDist.csv')
+
+        # Find unions with Sim
+        F = [1 for x in range(len(texts))]
+        result = ''
+        clusters = dict()
+        for i in range(len(texts)):
+            clusters[i] = [i]
+
+        for k in range(len(texts) - 1):
+            #printDist(dist, texts, self.filenames)
+            union, currSim = FindUnionSim(S, F)
+            firstCluster = ClusterByDoc(union[0], clusters)
+            secondCluster = ClusterByDoc(union[1], clusters)
+            print('found clusters = ' + str(firstCluster) + ' and ' + str(secondCluster))
+            # print(str(union[0] + 1) + ' + ' + str(union[1] + 1) + ' with dist= ' + str(sim[union[0]][union[1]]))
+            result += 'Step' + str(k) + '\nUnion --->;' + Cluster2String(clusters, firstCluster) + ';+;' \
+                      + Cluster2String(clusters, secondCluster) + ';=;'
+            UnionClusters(firstCluster, secondCluster, clusters)
+            result += Cluster2String(clusters, ClusterByDoc(union[0], clusters)) + ';sim = ' + str(currSim) + '\n'
+            result += Cluster2StringNames(clusters, ClusterByDoc(union[0], clusters), self.filenames) + '\n'
+
+            # doc2cluster[union[1]] = '{' + doc2cluster[union[1]] + ',' + doc2cluster[union[0]] + '}'
+            # doc2cluster[union[0]] = ''
+
+            F[union[0]] = 0
+            # F[union[1]] = 0
+            new_sim = [[0 for x in range(len(texts))] for y in range(len(texts))]
+            for i in range(len(texts)):
+                for j in range(len(texts)):
+                    new_sim[i][j] = S[i][j]
+
+            for j in range(len(texts)):
+                for i in range(2):
+                    new_sim[j][union[i]] = 0.5 * (S[j][union[0]]) + 0.5 * (S[j][union[1]])
+
+            S = new_sim
+
+        writeStringToFile(result.replace('\n ', '\n'), output_dir + 'stepsSim.csv')
 
         for i in range(len(texts)):
             self.textEdit.append(str(doc2cluster[i])+'\n')
             print(doc2cluster[i])
+
+    def makeClasterizationKMiddle(self):
+        self.textEdit.setText("")
+        self.configurations["minimal_word_size"] = self.spinBoxMinimalWordsLen.value()
+        self.textEdit.append('Кластеризация к-средних' + '\n')
+        texts = makePreprocessing(self.filenames, self.morph, self.configurations, self.textEdit)
+        output_dir = self.configurations.get("output_files_directory", "output_files") + "/clasterization/KMiddle/"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        result = ''
+        #легенда в ответ
+        for doc in range(len(self.filenames)):
+            result += str(doc) + ' = ' + os.path.basename(self.filenames[doc]) + '\n'
+        result += '\n'
+
+        # Нахождение матрицы весов
+        self.textEdit.append('Нахождение матрицы весов' + '\n')
+        t_all = dict()
+
+        for text in texts:
+            for key, value in text.sorted_word_frequency:
+                t_all[key] = t_all.get(key, 0) + 1
+
+        # Найти df
+        df_string = ''
+        df_string = df_string + "Слово;Используется в документах\n"
+        for key, value in t_all.items():
+            df_string = df_string + key + ';' + str(value) + '\n'
+        writeStringToFile(df_string.replace('\n ', '\n'), output_dir + 'df.csv')
+
+        W = [[0 for x in range(len(t_all))] for y in range(len(texts))]
+        print('len(texts)=' + str(len(texts)))
+        print('len(t_all)=' + str(len(t_all)))
+        W_norm = [0 for x in range(len(texts))]
+        i = 0
+        j = 0
+        for row in range(len(texts)):
+            j = 0
+            for key, value in t_all.items():
+                text = texts[row]
+                if (key in text.word_frequency):
+                    frequency_in_this_doc = text.word_frequency[key]
+                else:
+                    frequency_in_this_doc = 0
+                W[i][j] = frequency_in_this_doc * math.log10(len(texts) / value)
+                W_norm[i] += math.pow(W[i][j], 2)
+                # print('W[' + key + '][' + filenames[i] + '] = ' + str(frequency_in_this_doc) + '*Log(' + str(
+                #    len(texts)) + '/' + str(value) + ') = ' + str(W[i][j]))
+
+                j += 1
+            W_norm[i] = math.sqrt(W_norm[i])
+            print('wnorm = ' + str(W_norm[i]))
+            i += 1
+
+        for i in range(len(texts)):
+            for j in range(len(t_all)):
+                W[i][j] /= W_norm[i]
+
+        W_string = ''
+        W_string = W_string + "Нормированные веса\n"
+        for key, value in t_all.items():
+            W_string = W_string + ';' + key
+        W_string += '\n'
+        i = 0
+        for row in W:
+            W_string += self.filenames[i]
+            for item in row:
+                W_string = W_string + ';' + str(round(item, 10))
+            W_string += '\n'
+            i += 1
+        writeStringToFile(W_string.replace('\n ', '\n'), output_dir + 'W.csv')
+
+        S = GetS(W)
+        sim_string = ''
+        for name in self.filenames:
+            sim_string = sim_string + ';' + name
+        sim_string += '\n'
+        for i in range(len(texts)):
+            sim_string += self.filenames[i]
+            for j in range(len(t_all)):
+                sim_string = sim_string + ';' + str(S[i][j])
+            sim_string += '\n'
+        writeStringToFile(sim_string.replace('\n ', '\n'), output_dir + 'sim.csv')
+
+        n = len(texts)
+        m = len(texts)
+        S = [[0 for x in range(n)] for y in range(m)]
+
+        for i in range(n):
+            for j in range(m):
+                summ = 0
+                for k in range(len(t_all)):
+                    summ += math.pow(W[i][k] - W[j][k], 2)
+                S[i][j] = math.sqrt(summ)
+
+        dist_string = ''
+
+        for name in self.filenames:
+            dist_string = dist_string + ';doc' + str(name)[str(name).find('/') + 1:str(name).find('.')]
+        dist_string += '\n'
+        for i in range(len(texts)):
+            dist_string += 'doc' + str(self.filenames[i])[
+                                   str(self.filenames[i]).find('/') + 1:str(self.filenames[i]).find('.')]
+            for j in range(len(texts)):
+                dist_string = dist_string + ';' + str(round(S[i][j], 2))
+            dist_string += '\n'
+        writeStringToFile(dist_string.replace('\n ', '\n').replace('.', ','), output_dir + 'dist.csv')
+
+        # Проверим для каждого уровня
+        for centroidCount in range(len(texts), 0, -1):
+            result += 'Кол-во кастеров - ' + str(i) + '\n'
+
+            clusterCenteroids = dict()
+            # Шаг 1. Инициализация центров кластеров $, j = 1,k, например, случайными числами.
+            clusterCenteroids = [[random.randrange(0, 100, 1)/10000 for x in range(len(t_all))] for y in range(centroidCount)]
+
+            #Шаг 2. Cj={} , j=1,k.
+            doc2cluster = [0 for x in range(len(texts))]
+            for i in range(len(texts)):
+                doc2cluster[i] = -1
+
+            for i in range(len(texts)):
+                clusterDist = [[0 for x in range(len(texts))] for y in range(centroidCount)]
+
+            # Находим таблицу Dist для центроидов
+            for i in range(centroidCount):
+                for j in range(len(texts)):
+                    summ = 0
+                    for k in range(len(t_all)):
+                        summ += math.pow(W[j][k] - clusterCenteroids[i][k], 2)
+                        clusterDist[i][j] = math.sqrt(summ)
+
+            while True:
+                changes = False
+                #Шаг 3. Для каждого  di∈ D:
+
+                for doc in range(len(texts)):
+                    minDistance= 9999
+                    minCluster = -1
+                    currentDistance = 0
+                    for cluster in range(0, centroidCount):
+                        #for dist in range(len(texts)-1):
+                            currentDistance= abs(clusterDist[cluster][doc])
+                            #currentDistance/=len(t_all)
+                            if (currentDistance<minDistance):
+                                minDistance = currentDistance
+                                minCluster = cluster
+                    if(doc2cluster[doc] != minCluster):
+                        doc2cluster[doc] = minCluster
+                        changes = True
+
+                docCount=0
+                newDistance = 0
+                for cluster in range(centroidCount):
+                    summ = 0
+
+                    for doc in range(len(texts)-1):
+                        if(doc2cluster[doc] == cluster):
+                            if(docCount==0):
+                                clusterCenteroids[cluster] = [0 for x in range(len(t_all))]
+                            docCount+=1
+                            for k in range(len(t_all)):
+                                clusterCenteroids[cluster][k] += W[cluster][k]
+                    if(docCount!=0):
+                        for k in range(len(t_all)):
+                            clusterCenteroids[cluster][k] /= docCount
+                    docCount=0
+
+                # Находим таблицу Dist для центроидов
+                for i in range(centroidCount):
+                    for j in range(len(texts)):
+                        summ = 0
+                        for k in range(len(t_all)):
+                            summ += math.pow(W[j][k] - clusterCenteroids[i][k], 2)
+                            clusterDist[i][j] = math.sqrt(summ)
+
+                if(changes==False):
+                    print("END")
+                    #запишем результаты
+                    #result += 'Кластеров -'+ str(centroidCount) + '\n'
+                    for cluster in range (centroidCount):
+                        result += ';Кластер'+ str(cluster)
+                        for doc in range (len(texts)):
+                            if (doc2cluster[doc] == cluster):
+                                result += '; ' + str(doc)
+                        result += '\n'
+                    result += '\n'
+                    break
+
+        writeStringToFile(result.replace('\n ', '\n'), output_dir + 'steps.csv')
+        self.textEdit.append('Кластеризация к-средних завершена' + '\n')
