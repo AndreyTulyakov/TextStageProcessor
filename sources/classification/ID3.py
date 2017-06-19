c290b62249821784afd6ae7a165a13af280ac8f6
Andrey Tulyakov <mhyhre@gmail.com>
2017-06-19 10:16:35 +0300

    Немного исправил метод, - Артём.

--- sources/classification/ID3.py
+++ sources/classification/ID3.py
@@ -17,18 +17,17 @@ import pymorphy2
 ############################################
 
 
-
 category = []
 trueFiles = []
-falseFiles=[]
+falseFiles = []
 testFiles = []
 trueFilesName = []
-falseFilesName=[]
+falseFilesName = []
 testFilesName = []
 input_dir1 = ''
 
-def Classification_Text_ID3(input_dir,output_dir, trainingSet, trainingClass, testSet):
 
+def Classification_Text_ID3(input_dir, output_dir, trainingSet, trainingClass, testSet):
     global category
     global trueFiles
     global falseFiles
@@ -38,61 +37,72 @@ def Classification_Text_ID3(input_dir,output_dir, trainingSet, trainingClass, te
     global testFilesName
     global input_dir1
 
+    # f = open('SET.txt','w')
+    # f.write(str(trainingSet))
+    # f = open('CLASS.txt', 'w')
+    # f.write(str(trainingClass))
+    # f = open('TESTSET.txt', 'w')
+    # f.write(str(testSet))
+
     tree_file = output_dir + 'tree.txt'
     result_file = output_dir + 'result.txt'
     category = sorted(list(set(trainingClass)))
     input_dir1 = input_dir
     testFiles = testSet
 
-
-
     for i in range(len(trainingSet)):
-       if  trainingClass[i] == category[0]:
-           trueFiles.append(trainingSet[i])
-       else:
-           falseFiles.append(trainingSet[i])
+        if trainingClass[i] == category[0]:
+            trueFiles.append(trainingSet[i])
+        else:
+            falseFiles.append(trainingSet[i])
 
     Words = words()
     list_words_in_file = Analysis_of_words(Words)
-    applyID3(tree_file,Words,list_words_in_file)
+    applyID3(tree_file, Words, list_words_in_file)
     testTrue = []
     testFalse = []
-    lastWord = ''
+    lastWord = ' '
     i = 0
     for f in testFiles:
-        #f = open(directoryTest + '/' + f ,'r')
-        #arr = list(set(f.readline().split()))'
         arr = list(set(f))
         with open(tree_file) as tree:
             for line in tree:
                 s1 = line.strip()
                 s1 = s1.split('=')
-                if s1[0]==lastWord:
-                    lastWord = ''
+                if s1[0] == lastWord:
+                    lastWord = ' '
                     continue
-                if s1[0] in arr or lastWord != '' :
+                if s1[0] in arr or lastWord != ' ':
                     continue
                 else:
                     if s1[0] == '0':
                         testFalse.append(testFilesName[i])
-                        i+=1
+                        i += 1
                         break
                     if s1[0] == '1':
                         testTrue.append(testFilesName[i])
-                        i+=1
+                        i += 1
                         break
                     else:
-                        lastWord == str(s1[0])
+                        a = s1[0]
+                        lastWord = a
                         continue
-    res = open(result_file,'w')
+    res = open(result_file, 'w')
 
     for i in range(len(testTrue)):
-        res.write(str(testTrue[i]) + ' ')
-        if i == len(testTrue) -1 : res.write('- ' + category[1] + '\n')
+        if i == 0:
+            res.write('\n' + category[0] + ":")
+            print('\n' + category[0] + ":")
+        res.write(str('\n\t' + testTrue[i]))
+        print(str('\t' + testTrue[i]))
     for i in range(len(testFalse)):
-        res.write(str(testFalse[i]) + ' ')
-        if i == len(testFalse) - 1: res.write('- ' + category[0] + '\n')
+        if i == 0:
+            res.write('\n' + category[1] + ":")
+            print('\n' + category[1] + ":")
+        res.write(str('\n\t' + testFalse[i]))
+        print(str('\t' + testFalse[i]))
     res.close()
+
 
 def words():
     Words = {}
@@ -106,22 +116,23 @@ def words():
     global input_dir1
 
     for f in trueFiles:
-        #ff = open(directoryTrue + '/' + f1, 'r')
-        #arr = f
         arr = list(set(f))
         for s in arr:
-            if s not in Words:  Words[s] = [0,1,0.0]
-            else: Words[s][1]+=1
+            if s not in Words:
+                Words[s] = [0, 1, 0.0]
+            else:
+                Words[s][1] += 1
 
     for f in falseFiles:
-        #ff = open(directoryFalse + '/' + f2,'r')
         arr = f
         arr = list(set(arr))
         for s in arr:
-            if s not in Words:  Words[s] = [1,0,0.0]
-            else: Words[s][0]+=1
-    #ff.close()
+            if s not in Words:
+                Words[s] = [1, 0, 0.0]
+            else:
+                Words[s][0] += 1
     return Words
+
 
 def Analysis_of_words(Words):
     global category
@@ -139,23 +150,22 @@ def Analysis_of_words(Words):
 
     trueFilesName = os.listdir(input_dir1 + '/train/' + category[0])
     falseFilesName = os.listdir(input_dir1 + '/train/' + category[1])
-    testFilesName = os.listdir(input_dir1 + '/test/' + category[1]) +  os.listdir(input_dir1 + '/test/' + category[0])
+    testFilesName = sorted(
+        os.listdir(input_dir1 + '/test/' + category[0]) + os.listdir(input_dir1 + '/test/' + category[1]))
 
     for i in range(len(trueFiles)):
         for j in range(len(list_words) + 1): cells.append(0)
-        #ff = open(directoryTrue + '/' + f, 'r')
-        #arr = ff.readline().split()
+
         arr = trueFiles[i]
         arr = list(set(arr))
         for s in list_words:
             if s in arr: cells[list_words.index(s)] = 1
-        cells[len(cells)-1] = 1
+        cells[len(cells) - 1] = 1
         list_words_in_files[trueFilesName[i]] = cells
         cells = []
 
     for i in range(len(falseFiles)):
         for j in range(len(list_words) + 1): cells.append(0)
-        #ff = open(directoryFalse + '/' + f, 'r')
         arr = falseFiles[i]
         arr = list(set(arr))
         for s in list_words:
@@ -163,21 +173,10 @@ def Analysis_of_words(Words):
         list_words_in_files[falseFilesName[i]] = cells
         cells = []
 
-    for i in range(len(testFiles)):
-        for j in range(len(list_words) + 1): cells.append(0)
-        #ff = open(directoryTrue + '/' + f, 'r')
-        arr = testFiles[i]
-        arr = list(set(arr))
-        for s in list_words:
-            if s in arr: cells[list_words.index(s)] = 1
-        cells[len(cells)-1] = 1
-        list_words_in_files[testFilesName[i]] = cells
-        cells = []
-    #ff.close()
-
     return list_words_in_files
 
-def ParseAttributes2(Words,list_words_in_file):
+
+def ParseAttributes2(Words, list_words_in_file):
     global category
     global trueFiles
     global falseFiles
@@ -196,94 +195,86 @@ def ParseAttributes2(Words,list_words_in_file):
         attr[fWords[0]] = [i, fWords[1], fWords[2]]
         attr[fWords[1]] = 1
         attr[fWords[2]] = 0
-        i+=1
-    num = attrnum -1 # позиция вывода
-    #testnum = len(trueFiles) + len(falseFiles)
+        i += 1
+    num = attrnum - 1  # позиция вывода
+    # testnum = len(trueFiles) + len(falseFiles)
 
     for s in justList.keys():
         tests.append(justList[s])
     return [attrnum, sorted(attrnames), attr, tests, num]
 
-def entropy(tests,num):
-    import math
-    def log2(x): return math.log(x)/math.log(2)
-    neg = float(len(list(filter(lambda x:(x[num]==0),tests))))
-    tot = float(len(tests))
-    if ((neg==tot) or (neg==0)): return 0
-    return -(neg/tot)*log2(neg/tot)-((tot-neg)/tot)*log2(((tot-neg)/tot))
 
-def gain(tests,attrnum,num):
+def entropy(tests, num):
+    import math
+    def log2(x): return math.log(x) / math.log(2)
+
+    neg = float(len(list(filter(lambda x: (x[num] == 0), tests))))
+    tot = float(len(tests))
+    if ((neg == tot) or (neg == 0)): return 0
+    # e = -(neg/tot)*log2(neg/tot)-((tot-neg)/tot)*log2(((tot-neg)/tot))
+    return -(neg / tot) * log2(neg / tot) - ((tot - neg) / tot) * log2(((tot - neg) / tot))
+
+
+def gain(tests, attrnum, num):
     res = 0
     for i in range(2):
-        arr = list(filter(lambda x:(x[attrnum]==i),tests))
-        res += entropy(arr,num)*len(arr)/float(len(tests))
-    return entropy(tests,num)-res
+        arr = list(filter(lambda x: (x[attrnum] == i), tests))
+        res += entropy(arr, num) * len(arr) / float(len(tests))
+        # A = entropy(tests,num)-res
+    return entropy(tests, num) - res
 
-def ID3(tests,num,f,tabnum,usedattr,attrnames,attr):
-   # t = Analysis_of_words()
+
+def ID3(tests, num, f, tabnum, usedattr, attrnames, attr):
     mas = ['', '', '', '', ]
+
     def findgains(x):
         if usedattr[x]: return 0
-        return gain(tests,x,num)
-    if (len(tests)==0):
-        f.write('\t'*tabnum+'1')
+        return gain(tests, x, num)
+
+    if (len(tests) == 0):
+        f.write('\t' * tabnum + '1')
         return
-    if len(list(filter(lambda x:(x[num]==0),tests)))>len(list(filter(lambda x:(x[num]==1),tests))):
+    if len(list(filter(lambda x: (x[num] == 0), tests))) > len(list(filter(lambda x: (x[num] == 1), tests))):
         majority = '0'
-    else: majority = '1'
+    else:
+        majority = '1'
     gains = list(map(findgains, range(len(tests[0]))))
     maxgain = gains.index(max(gains))
-    if (gains [maxgain] == 0):
+    if (gains[maxgain] == 0):
         f.write('\t' * tabnum + majority + '\n')
-        # if Tree[lastName][1] == '': pos = 1
-        # else: pos = 3
-        # ar4 = Tree[lastName]
-        # ar4[pos] = majority
-        # Tree[lastName] = ar4
         return
     arrpos = list(filter(lambda x: (x[maxgain] == 1), tests))
     arrneg = list(filter(lambda x: (x[maxgain] == 0), tests))
     newusedattr = usedattr
     newusedattr[maxgain] = True
     f.write('\t' * tabnum + str(attrnames[maxgain] + '=' + str(attr[attrnames[maxgain]][1]) + '\n'))
-    # while True:
-    #     if attrnames[maxgain] not in Tree:
-    #         ar1 = mas
-    #         ar1[0] = attr[attrnames[maxgain]][1]
-    #         Tree[attrnames[maxgain]] = ar1
-    #         global lastName
-    #         if lastName == ' ':
-    #             lastName = attrnames[maxgain]
-    #         else:
-    #             ar3 = Tree[lastName]
-    #             ar3[1] = attrnames[maxgain]
-    #             Tree[lastName] = ar3
-    #             lastName = attrnames[maxgain]
-    #     else:
-    #         ar2 = Tree[attrnames[maxgain]]
-    #         ar2[2] =  attr[attrnames[maxgain]][1]
-    #         Tree[attrnames[maxgain]] = ar2
-    #     break
+
     if (len(arrpos) == 0):
         f.write('\t' * (tabnum + 1) + majority + '\n')
     else:
         ID3(arrpos, num, f, tabnum + 1, newusedattr, attrnames, attr)
     f.write('\t' * tabnum + str(attrnames[maxgain]) + '=' + str(attr[attrnames[maxgain]][2]) + '\n')
-    # ar5 = Tree[attrnames[maxgain]]
-    # ar5[2] = attr[attrnames[maxgain]][2]
-    # lastName = attrnames[maxgain]
     if (len(arrneg) == 0):
         f.write('\t' * (tabnum + 1) + majority + '\n')
     else:
         ID3(arrneg, num, f, tabnum + 1, newusedattr, attrnames, attr)
 
-def applyID3(outfname,Words,list_words_in_file):
-    bigarr = ParseAttributes2(Words,list_words_in_file)
+
+def applyID3(outfname, Words, list_words_in_file):
+    bigarr = ParseAttributes2(Words, list_words_in_file)
     attrnum, attrnames, attr, tests, num = bigarr[0], bigarr[1], bigarr[2], bigarr[3], bigarr[4]
     f = open(outfname, 'w')
     usedattr = []
     for i in range(attrnum): usedattr.append(i == num)
     ID3(tests, attrnum - 1, f, 0, usedattr, attrnames, attr)
 
-#########TEST############
-#Classification_Text_ID3('C:/Users/art-c/Desktop/TextStageProcessor-master/input_files/classification/china','C:/Users/art-c/Desktop/TextStageProcessor-master/output_files/classification/id3_out/', [['китайский', 'пекин', 'китайский'], ['китайский', 'китайский', 'шанхай'], ['китайский', 'китайский', 'макао'], ['токио', 'япония', 'китайский']], ['china', 'china', 'china', 'not_china'], [['китайский', 'китайский', 'китайский', 'токио', 'япония']])
+########TEST############
+
+# inputF = 'C:/Users/art-c/Desktop/TextStageProcessor-master/input_files/classification/china'
+# outputF = 'C:/Users/art-c/Desktop/TextStageProcessor-master/output_files/classification/id3_out/'
+# TESTSET = [['китайский', 'китайский', 'китайский', 'токио', 'япония']]
+# CLASS = ['china', 'china', 'china', 'not_china']
+# SET = [['китайский', 'пекин', 'китайский'], ['китайский', 'китайский', 'шанхай'], ['китайский', 'китайский', 'макао'], ['токио', 'япония', 'китайский']]
+
+# Classification_Text_ID3(inputF,outputF,SET,CLASS,TESTSET )
+
